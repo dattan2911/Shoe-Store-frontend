3c8cecfefe1bcacb7f88b61cdd56c0d9fbca2102
Dat Tan <dattan2911@gmail.com>
2023-02-19 16:03:16 +0700

    update

--- src/App.js
+++ src/App.js
@@ -149,7 +149,7 @@ function App() {
             <Nav.Item>
               <strong>Categories</strong>
             </Nav.Item>
-            {categories.map((category) => (
+            {categories.map(category => (
               <Nav.Item key={category}>
                 <LinkContainer
                   to={`/search/category=${category}`}
