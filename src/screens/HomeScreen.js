7be09a5ee9f509eaad1391822a3a37ebebf0920f
Dat Tan <dattan2911@gmail.com>
2023-02-20 20:50:56 +0700

    fix products

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -57,12 +57,12 @@ function HomeScreen() {
                         <MessageBox variant="danger">{error}</MessageBox>
                     ) : (
                         <Row>
-                            {products?.map((product) => (
+                            {products?products.map((product) => (
                                 <Col key={product.slug} sm={6} md={4} lg={3} className="mb-3">
                                     <Product product={product}></Product>
                                 </Col>
                             ))
-                            }
+                            :{}}
                         </Row>
                     )}
             </div>
